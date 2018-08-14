47cf2f00928a49cbaca5f306fc24658bc5dc1a24
Andrew Lechlak <AndrewLechlak@gmail.com>
2018-08-14 16:22:14 -0400

    Initial commit

---
+++ js/google-sheets-html.js
@@ -0,0 +1,39 @@
+/*!
+ * 
+ * Google Sheets To HTML v0.9a
+ * 
+ * To use, simply replace the "tq?key=" value in the
+ * URL below with your own unique Google document ID
+ * 
+ * The Google document's sharing must be set to public
+ * 
+ */
+
+google.load('visualization', '1', {
+    packages: ['table']
+});
+var visualization;
+
+function drawVisualization() {
+    var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=1I8YQrRBXyoxFXcRYKu-jZOpQ7cs8MGZnN6sQSDChSDw&output=html&usp=sharing');
+    query.setQuery('SELECT A, B, C, D, E label A "Room", B "Location", C "Lecture Style", D "Tables and Chairs", E "Assets"');
+    query.send(handleQueryResponse);
+    
+}
+
+function handleQueryResponse(response) {
+    if (response.isError()) {
+        alert('There was a problem with your query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+    var data = response.getDataTable();
+    visualization = new google.visualization.Table(document.getElementById('table'));
+    visualization.draw(data, {
+        allowHtml: true,
+        legend: 'bottom'
+    });
+
+    
+
+}
+google.setOnLoadCallback(drawVisualization);
